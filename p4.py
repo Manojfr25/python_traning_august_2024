5efe7f0b418f215cfca994eb2fa88cf0f2193884
manojfr25 <manojfr2005@gmail.com>
2024-08-20 17:34:25 +0530

    updated

--- p4.py
+++ p4.py
@@ -1,4 +1,5 @@
 # Program to accept 3 distinct number & find smallest number 
+
 '''numbers=[]
 for i in range(3):
    print('Enter number',str(i),': ')
@@ -6,12 +7,14 @@ for i in range(3):
    numbers.append(x)
 numbers.sort()
 print('Smallest number is: ',numbers[0])'''
+
+# Program to accept 3 distinct number & find smallest number 
 input_num1 = int(input('Enter first number'))
 input_num2 = int(input('Enter second number'))
 input_num3 = int(input('Enter third number'))
 if input_num1<input_num2 and input_num1<input_num3:
     print(input_num1 , 'is the smallest number')
-elif input_num2<input_num3 and input_num2<input_num1:
+if input_num2<input_num3 and input_num2<input_num1:
     print(input_num2 , 'is the smallest number')
-elif input_num3<input_num2 and input_num3<input_num1:
+if input_num3<input_num2 and input_num3<input_num1:
     print(input_num3 , 'is the smallest number')
