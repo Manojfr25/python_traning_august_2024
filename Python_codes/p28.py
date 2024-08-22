451fa338468e80c0c77630454539a93f3a57df22
manojfr25 <manojfr2005@gmail.com>
2024-08-22 16:22:21 +0530

    completed assignment programs

--- Python_codes/p28.py
+++ Python_codes/p28.py
@@ -6,9 +6,9 @@ if M<N:
     i=M
     prime_num_list=[]
     while M <= i <= N:
-        if i == 2 or i== 3 or i== 5 or i== 7 or i== 11 or i== 13:
+        if i == 2 or i== 3 or i== 5 or i== 7 or i== 11:
             prime_num_list.append(i)
-        elif i % 2 != 0 and i % 3 !=0 and i % 5 != 0 and i % 7 != 0 and i % 11 != 0 and i%13==0:
+        elif i % 2 != 0 and i % 3 !=0 and i % 5 != 0 and i % 7 != 0 and i % 11 != 0:
             prime_num_list.append(i)
         i += 1
     print(f"Prime number between {M} and {N} are: {prime_num_list}")
